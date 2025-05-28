0094570fe02233d658402fe37940ed5a4c765d17
k1 <keyvan.soltan18@gmail.com>
2025-05-28 20:48:49 +0330

    General functions

---
+++ general.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func isServerFree() bool {
+	jsonFile, err := os.Open("data/status.json")
+	defer jsonFile.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	var status map[string]interface{}
+	json.Unmarshal(byteValue, &status)
+
+	if status["status"] == "free" {
+		return true
+	}
+	return false
+}
+
+func setServerStatus(status string) error {
+	statusMap := make(map[string]string)
+	statusMap["status"] = status
+	jsonData, err := json.Marshal(statusMap)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile("data/status.json", jsonData, 0644)
+
+	return nil
+}
+
+// readLastOffset reads the last saved byte offset from the progress file.
+func readLastOffset(filePath string) int64 {
+    data, err := os.ReadFile(filePath)
+    if err != nil {
+        // If file doesn't exist or other read error, start from beginning
+        return 0
+    }
+    offset, err := strconv.ParseInt(strings.TrimSpace(string(data)), 10, 64)
+    if err != nil {
+        // If conversion error, start from beginning
+        fmt.Fprintf(os.Stderr, "Error parsing offset from %s: %v. Starting from beginning.\n", filePath, err)
+        return 0
+    }
+    fmt.Printf("Resuming from offset: %d\n", offset)
+    return offset
+}
+
+// writeCurrentOffset writes the current byte offset to the progress file.
+func writeCurrentOffset(filePath string, offset int64) error {
+    return os.WriteFile(filePath, []byte(strconv.FormatInt(offset, 10)), 0644)
+}
+
+

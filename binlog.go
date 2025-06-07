7df46086e08d40c088b27b8cd110c9821bbe6752
k1 <keyvan.soltan18@gmail.com>
2025-06-07 21:52:30 +0330

    Add a function to parse binlog coordinates files

--- binlog.go
+++ binlog.go
@@ -10,7 +10,7 @@ import (
 // BinlogPosition represents the structure of your JSON data
 type BinlogPosition struct {
 	Logfile string `json:"logfile"`
-	Logpos  int64  `json:"logpos"`
+	Logpos  uint32  `json:"logpos"`
 }
 
 // CompareBinlogFiles reads two files containing binlog positions
@@ -81,6 +81,27 @@ func CompareBinlogFiles(filePath1, filePath2 string) (string, error) {
 	return "", fmt.Errorf("binlog positions are identical") // Or handle as "same"
 }
 
-func getMainBinlogPositionFilePath() (string) {
+func GetMainBinlogPositionFilePath() (string) {
    return "data/main-binlog-position.json" 
+}
+
+func ParseBinlogCoordinatesFile(filePath string) (BinlogPosition, error) {
+	data, err := os.ReadFile(filePath)
+
+	// Check for read errors (excluding file not exists for emptiness check)
+	if err != nil && !os.IsNotExist(err) {
+        return BinlogPosition{}, fmt.Errorf("error reading file %s: %w", filePath, err)
+	}
+
+	empty := len(strings.TrimSpace(string(data))) == 0 || os.IsNotExist(err)
+
+	if empty{
+		return BinlogPosition{}, fmt.Errorf("file is empty or does not exist")
+	}
+
+	var pos BinlogPosition
+	if err := json.Unmarshal(data, &pos); err != nil {
+		return BinlogPosition{}, fmt.Errorf("error unmarshalling JSON from %s: %w", filePath, err)
+	}
+    return pos, nil
 }

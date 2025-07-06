6c87fc3478cd0091fbd6fa565d62f7c31683a32f
K1 <K1>
2025-07-06 17:43:11 +0330

    status functions

--- general.go
+++ general.go
@@ -9,35 +9,6 @@ import (
 	"strings"
 )
 
-func isServerFree() bool {
-	jsonFile, err := os.Open("data/status.json")
-	if err != nil {
-		MainLogger.Debug(err.Error())
-		panic(err)
-	}
-	defer jsonFile.Close()
-	byteValue, _ := io.ReadAll(jsonFile)
-
-	var status map[string]interface{}
-	json.Unmarshal(byteValue, &status)
-
-	if status["status"] == "free" {
-		return true
-	}
-	return false
-}
-
-func setServerStatus(status string) error {
-	statusMap := make(map[string]string)
-	statusMap["status"] = status
-	jsonData, err := json.Marshal(statusMap)
-	if err != nil {
-		return err
-	}
-	err = os.WriteFile("data/status.json", jsonData, 0644)
-
-	return nil
-}
 
 // readLastOffset reads the last saved byte offset from the progress file.
 func readLastOffset(filePath string) int64 {
---
+++ internal/util/status.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+func IsServerFree() bool {
+	jsonFile, err := os.Open("data/status.json")
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	var status map[string]interface{}
+	json.Unmarshal(byteValue, &status)
+
+	if status["status"] == "free" {
+		return true
+	}
+	return false
+}
+
+func SetServerStatus(status string) error {
+	statusMap := make(map[string]string)
+	statusMap["status"] = status
+	jsonData, err := json.Marshal(statusMap)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile("data/status.json", jsonData, 0644)
+
+	return nil
+}

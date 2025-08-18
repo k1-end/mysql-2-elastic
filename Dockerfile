8a178f220593116ab19292c9847bbc9fe29d2958
K1 <K1>
2025-08-18 22:37:56 +0330

    Fix running seed.bash

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,10 @@ COPY go.mod go.sum ./
 RUN go mod download
 
 RUN apk add --no-cache mysql-client && \
+    apk add --no-cache jq && \
     apk add --no-cache mariadb-connector-c vim
 
 COPY . .
-RUN go build -v -o /usr/local/bin/app ./...
+RUN go build -v -o /usr/local/bin/app .
 
 CMD ["app"]
--- seed.bash
+++ seed.bash
@@ -4,4 +4,4 @@ password=$(echo "$json_config" | jq -r '.database.password')
 port=$(echo "$json_config" | jq -r '.database.port')
 host=$(echo "$json_config" | jq -r '.database.host')
 world_database_path=$(echo "$json_config" | jq -r '.world_database_path')
-mysql --host=$host --user=$user --password=$password --port=$port < $world_database_path 
+/usr/bin/mariadb --skip-ssl-verify-server-cert --host=$host --user=$user --password=$password --port=$port < $world_database_path 
